82a939c1a6837300ba3d1ed1679dcb7acb223294
timcol1 <avlyakulovtimur@gmail.com>
2023-11-14 14:00:03 +0200

    add example with docker in docker

--- test/script.sh
+++ test/script.sh
@@ -1,10 +1,5 @@
-name=$1
-len=${#name}
-first_letter=${name::1}
-rest_letters=${name:1:len}
-if [ -z "$name" ]; then
-    echo "Нужно ввести аргумент"
+if [ ! -e names.txt ]; then
+    echo "Не могу найти файл 'names.txt'"
     exit 1
-else
-  echo "Hello, ${first_letter^}${rest_letters,,} :)"
 fi
+cat names.txt | awk '{print "Hello, "$1}'
